7b14564e927cdf863db980d404fcfd5e8fac7edf
Ogge Norin <oskar_norin@hotmail.com>
2017-05-04 21:18:49 +0000

    added test for email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates :first_name, :last_name,  
                   format: { with: /\A[a-zA-Z]+( [a-zA-Z])*/  }                                  
   
-  REGEXP = /\A[^@]+@[a-z\d\-]+(\.[a-z])*\.[a-z]{2,3}\i/
-  validates :email, format: REGEXP
+  EMAIL_REGEXP = /\A[^@\s]+@[a-z\d\-]+(\.[a-z])*\.[a-z]{2,3}\z/i
+  validates :email, format: { with: EMAIL_REGEXP }
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(first_name: "Valid fname", last_name: "Valid lname",
+                              email: "valid@email.com", password: "password",
+                              password_confirmation: "password")
+  end
+  
+  test "Should reject invalid email addresses" do
+    invalid_emails = ["@.com", "asd.com", "@",
+                                    "example@asd-com", "asd.asd@example..com"]
+    invalid_emails.each do |email|
+      @user.email = email
+      assert_not @user.valid?
+    end
+  end
+  
+  test "Should accept valid email addresses" do
+    valid_emails = ["valid@example.com", "asdas-asd@jacob.com", 
+                          "ole@ole-ole.com", "ASDASD@ole.com"]
+    valid_emails.each do |email|
+      @user.email = email
+      assert @user.valid?, "#{email} should be valid"
+    end
+  end
+  
 end
