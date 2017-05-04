76f38ed93c1b35bd754ad86dcf1bce99fa90f9a8
Ogge Norin <oskar_norin@hotmail.com>
2017-05-04 20:54:06 +0000

    Added validation to user, and new action

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   
   has_secure_password # Method from BCrypt gem.
   
+  validates :first_name, :last_name, :email, presence: true
+  
+  validates :first_name, :last_name,  
+                  format: { with: /\A[a-zA-Z]+( [a-zA-Z])*/  }                                  
+  
+  REGEXP = /\A[^@]+@[a-z\d\-]+(\.[a-z])*\.[a-z]{2,3}\i/
+  validates :email, format: REGEXP
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  
-  get 'users/new'
-
-  get 'users/create'
 
   root to: 'static_pages#home'
   get 'about', to: 'static_pages#about'
